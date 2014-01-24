7a587ad8786ccf72506d9f5cacc5d2c0d6c70136
Bastian Krol <bastian.krol@web.de>
2014-01-24 17:35:56 +0100

    move todos to separate file

--- src/game.js
+++ src/game.js
@@ -1,39 +1,3 @@
-/*
- * TODO
- * - [UX] center map on screen
- * - Get rid of level.maxHeight attribute
- * - Get rid of level.baseHeight attribute - just center map on screen
- *
- * - [TESTS] For z-index with various heights (probably partly broken)
- *
- * - [!] Scenes: welcome scene, level select scene, playing scene
- * - [!] Asset Pre-Loading
- * - [TESTS] For pixel positions during bot movements
- * - REVIEW TODOS!
- * - [REFACTORING] extract into components/entities:
- *   - source panel
- *   - program panel
- *   - each program area
- *   - button area
- * - [UX] remove cards by click
- * - [UX] when removing cards from instruction area, all other cards after should go
- *   to their predecessor
- * - [UX] when dropping cards in occupied spaces, insert them instead of replace
- *   them. Cards get destroyed if the move out at the end.
- * - [VISUAL] better colors for active and inactive instruction area
- * - [VISUAL] correct instruction area and slot sizes, so that cards fit perfectly
- * - [VISUAL] put a opaque div below the 'Level Solved!' message
- * - [VISUAL] make icons for buttons
- * - [VISUAL] Animate bot jump even when its jumping in situ
- * - [FEATURE] let level definition dictate which cards are available
- * - [FEATURE] level editor
- * - [FEATURE] save levels to local storage
- * - [FEATURE] load levels from local storage
- * - [FEATURE] export/import levels from local storage to/from file
- * - [FEATURE] Clicking execute/reset program cards *cancel* current
- *   execution - similar to _hasWon check
- */
-
 game = (function() {
   'use strict';
 
